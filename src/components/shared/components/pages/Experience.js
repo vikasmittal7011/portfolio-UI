ecf9be147af7f088406ed08a35824e5625aca901
vikas gupta <vikasaggrawal700@gmail.com>
2023-07-13 16:08:00 +0530

    add experience

---
+++ src/components/shared/components/pages/Experience.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Experience() {
+  return (
+    <div className="container my-5">
+      <h1>
+        As Professional I am a <span style={{ color: "#cd5d5d" }}>Fresher</span>
+      </h1>
+      <p className="fs-5 my-3">
+        Highly motivated and enthusiastic MERN stack developer seeking an
+        opportunity to leverage my skills and knowledge to contribute to a
+        dynamic and innovative software development team. Committed to
+        delivering high-quality web applications while continuously expanding my
+        expertise in the MERN stack.
+      </p>
+    </div>
+  );
+}
+
+export default Experience;
